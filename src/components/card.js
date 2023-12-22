eb13fc766a7ab78ee427374b87732c412e23c268
welpodron <welpodron@gmail.com>
2023-12-22 13:16:34 +0300

    Существенные изменения для первой итерации по последнему спринту

--- src/components/card.js
+++ src/components/card.js
@@ -1,28 +1,60 @@
-const createCard = (template, data, onDelete, onLike, onImageClick) => {
+const createCard = ({
+  currentUserId,
+  template,
+  data,
+  onDelete,
+  onLike,
+  onImageClick,
+}) => {
   const element = template.querySelector('.card').cloneNode(true);
 
   const image = element.querySelector('.card__image');
-  image.addEventListener('click', () => onImageClick(data.name, data.link));
+  image.addEventListener('click', () =>
+    onImageClick({
+      cardName: data.name,
+      cardLink: data.link,
+    })
+  );
   image.src = data.link;
   image.alt = data.name;
 
   element.querySelector('.card__title').textContent = data.name;
 
-  element
-    .querySelector('.card__delete-button')
-    .addEventListener('click', onDelete);
+  const counter = element.querySelector('.card__like-counter');
 
-  element.querySelector('.card__like-button').addEventListener('click', onLike);
+  if (data.likes.length) {
+    counter.classList.add('card__like-counter_is-active');
+    counter.textContent = data.likes.length;
+  }
+
+  const deleteButton = element.querySelector('.card__delete-button');
+
+  if (data.owner['_id'] === currentUserId) {
+    deleteButton.classList.add('card__delete-button_is-active');
+    deleteButton.addEventListener('click', () => {
+      onDelete({
+        cardId: data['_id'],
+        cardElement: element,
+        buttonElement: deleteButton,
+      });
+    });
+  }
+
+  const likeButton = element.querySelector('.card__like-button');
+
+  if (data.likes.find((element) => element['_id'] === currentUserId)) {
+    likeButton.classList.add('card__like-button_is-active');
+  }
+
+  likeButton.addEventListener('click', () => {
+    onLike({
+      cardId: data['_id'],
+      buttonElement: likeButton,
+      counterElement: counter,
+    });
+  });
 
   return element;
 };
 
-const deleteCard = (event) => {
-  event.target.closest('.card').remove();
-};
-
-const likeCard = (event) => {
-  event.currentTarget.classList.toggle('card__like-button_is-active');
-};
-
-export { deleteCard, createCard, likeCard };
+export { createCard };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,34 @@
 import './index.css';
 
-import { INITIAL_CARDS } from '../components/cards.js';
-
 import {
   closeModal,
   openModal,
   handleModalClick,
 } from '../components/modal.js';
 
-import { createCard, deleteCard, likeCard } from '../components/card.js';
+import { createCard as DOMCreateCard } from '../components/card.js';
+
+import {
+  getInitialCards as APIGetInitialCards,
+  getUserInfo as APIGetUserInfo,
+  updateUserAvatar as APIUpdateUserAvatar,
+  updateUserInfo as APIUpdateUserInfo,
+  likeCard as APILikeCard,
+  unLikeCard as APIUnLikeCard,
+  createCard as APICreateCard,
+  deleteCard as APIDeleteCard,
+} from '../components/api.js';
+
+import { clearValidation, enableValidation } from '../components/validation.js';
+
+const VALIDATION_CONFIG = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+};
 
 const popupImage = document.querySelector('.popup_type_image');
 const popupImageCaption = popupImage.querySelector('.popup__caption');
@@ -18,63 +38,211 @@ const cardsContainer = document.querySelector('.places__list');
 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardForm = document.forms['new-place'];
+const cardFormSubmitButton = cardForm.querySelector('.popup__button');
 const cardNameInput = cardForm.elements['place-name'];
 const cardLinkInput = cardForm.elements.link;
 
 const popupCard = document.querySelector('.popup_type_new-card');
 const popupCardButtonOpen = document.querySelector('.profile__add-button');
 
+const profileImageForm = document.forms['edit-avatar'];
+const profileImageInput = profileImageForm.elements.avatar;
+const profileImageFormSubmitButton =
+  profileImageForm.querySelector('.popup__button');
+
+const popupProfileImage = document.querySelector('.popup_type_edit-avatar');
+
+const profileImage = document.querySelector('.profile__image');
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
 const profileForm = document.forms['edit-profile'];
+const profileFormSubmitButton = profileForm.querySelector('.popup__button');
 const profileNameInput = profileForm.elements.name;
 const profileDescriptionInput = profileForm.elements.description;
 
 const popupProfile = document.querySelector('.popup_type_edit');
 const popupProfileButtonOpen = document.querySelector('.profile__edit-button');
 
-const handleCardImageClick = (cardName, cardLink) => {
-  popupImageImage.src = cardLink;
-  popupImageImage.alt = cardName;
-  popupImageCaption.textContent = cardName;
+const popupConfirm = document.querySelector('.popup_type_confirm');
+const popupConfirmButton = popupConfirm.querySelector('.popup__button_confirm');
 
-  openModal(popupImage);
+const setProfile = ({ name, description, avatar }) => {
+  profileName.textContent = name;
+  profileDescription.textContent = description;
+  profileImage.style.backgroundImage = `url(${avatar})`;
+};
+
+const toggleSubmitButtonState = ({ buttonElement, isSubmitting }) => {
+  if (isSubmitting) {
+    buttonElement.disabled = true;
+    buttonElement.textContent = 'Сохранение...';
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.textContent = 'Сохранить';
+  }
+};
+
+const handleCardLike = ({ cardId, buttonElement, counterElement }) => {
+  buttonElement.disabled = true;
+
+  if (buttonElement.classList.contains('card__like-button_is-active')) {
+    APIUnLikeCard(cardId)
+      .then(({ likes }) => {
+        buttonElement.classList.remove('card__like-button_is-active');
+
+        if (likes.length) {
+          counterElement.classList.add('card__like-counter_is-active');
+          counterElement.textContent = likes.length;
+        } else {
+          counterElement.classList.remove('card__like-counter_is-active');
+          counterElement.textContent = '';
+        }
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        buttonElement.disabled = false;
+      });
+  } else {
+    APILikeCard(cardId)
+      .then(({ likes }) => {
+        buttonElement.classList.add('card__like-button_is-active');
+
+        counterElement.classList.add('card__like-counter_is-active');
+        counterElement.textContent = likes.length;
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        buttonElement.disabled = false;
+      });
+  }
+};
+
+const handleCardDelete = ({ cardId, buttonElement }) => {
+  openModal(popupConfirm);
+  // так как по сути кнопка "Да" все равно выполняет только ровно одну операцию как confirm()
+  popupConfirmButton.onclick = () => {
+    buttonElement.disabled = true;
+
+    APIDeleteCard(cardId)
+      .then(() => {
+        buttonElement.closest('.card').remove();
+      })
+      .catch((error) => {
+        buttonElement.disabled = false;
+        console.error(error);
+      })
+      .finally(() => {
+        closeModal(popupConfirm);
+      });
+  };
 };
 
 const handleCardFormSubmit = (event) => {
   event.preventDefault();
 
-  cardsContainer.prepend(
-    createCard(
-      cardTemplate,
-      {
-        name: cardNameInput.value,
-        link: cardLinkInput.value,
-      },
-      deleteCard,
-      likeCard,
-      handleCardImageClick
-    )
-  );
+  toggleSubmitButtonState({
+    buttonElement: cardFormSubmitButton,
+    isSubmitting: true,
+  });
 
-  cardForm.reset();
+  APICreateCard({
+    name: cardNameInput.value,
+    link: cardLinkInput.value,
+  })
+    .then((cardData) => {
+      cardsContainer.prepend(
+        DOMCreateCard({
+          currentUserId: cardData.owner['_id'],
+          template: cardTemplate,
+          data: cardData,
+          onDelete: handleCardDelete,
+          onLike: handleCardLike,
+          onImageClick: handleCardImageClick,
+        })
+      );
 
-  closeModal(popupCard);
+      cardForm.reset();
+
+      closeModal(popupCard);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      toggleSubmitButtonState({
+        buttonElement: cardFormSubmitButton,
+        isSubmitting: false,
+      });
+    });
 };
 
 const handleProfileFormSubmit = (event) => {
   event.preventDefault();
 
-  profileName.textContent = profileNameInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
+  toggleSubmitButtonState({
+    buttonElement: profileFormSubmitButton,
+    isSubmitting: true,
+  });
 
-  closeModal(popupProfile);
+  APIUpdateUserInfo({
+    name: profileNameInput.value,
+    description: profileDescriptionInput.value,
+  })
+    .then(({ name, about, avatar }) => {
+      setProfile({
+        name,
+        description: about,
+        avatar,
+      });
+
+      closeModal(popupProfile);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      toggleSubmitButtonState({
+        buttonElement: profileFormSubmitButton,
+        isSubmitting: false,
+      });
+    });
+};
+
+const handleProfileImageFormSubmit = (event) => {
+  event.preventDefault();
+
+  toggleSubmitButtonState({
+    buttonElement: profileImageFormSubmitButton,
+    isSubmitting: true,
+  });
+
+  APIUpdateUserAvatar(profileImageInput.value)
+    .then(({ name, about, avatar }) => {
+      setProfile({
+        name,
+        description: about,
+        avatar,
+      });
+
+      closeModal(popupProfileImage);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+    .finally(() => {
+      toggleSubmitButtonState({
+        buttonElement: profileImageFormSubmitButton,
+        isSubmitting: false,
+      });
+    });
 };
 
 const handlePopupProfileButtonOpenClick = () => {
   profileNameInput.value = profileName.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
+
+  clearValidation(profileForm, VALIDATION_CONFIG);
 
   openModal(popupProfile);
 };
@@ -82,14 +250,38 @@ const handlePopupProfileButtonOpenClick = () => {
 const handlePopupCardButtonOpenClick = () => {
   cardForm.reset();
 
+  clearValidation(cardForm, VALIDATION_CONFIG);
+
   openModal(popupCard);
+};
+
+const handleCardImageClick = ({ cardName, cardLink }) => {
+  popupImageImage.src = cardLink;
+  popupImageImage.alt = cardName;
+  popupImageCaption.textContent = cardName;
+
+  openModal(popupImage);
+};
+
+const handleProfileImageClick = () => {
+  profileImageForm.reset();
+
+  clearValidation(profileImageForm, VALIDATION_CONFIG);
+
+  openModal(popupProfileImage);
 };
 
 cardForm.addEventListener('submit', handleCardFormSubmit);
 
 profileForm.addEventListener('submit', handleProfileFormSubmit);
 
+profileImageForm.addEventListener('submit', handleProfileImageFormSubmit);
+
 popupImage.addEventListener('click', handleModalClick);
+
+popupProfileImage.addEventListener('click', handleModalClick);
+
+profileImage.addEventListener('click', handleProfileImageClick);
 
 popupCard.addEventListener('click', handleModalClick);
 popupCardButtonOpen.addEventListener('click', handlePopupCardButtonOpenClick);
@@ -100,14 +292,31 @@ popupProfileButtonOpen.addEventListener(
   handlePopupProfileButtonOpenClick
 );
 
-INITIAL_CARDS.forEach((cardData) => {
-  cardsContainer.append(
-    createCard(
-      cardTemplate,
-      cardData,
-      deleteCard,
-      likeCard,
-      handleCardImageClick
-    )
-  );
-});
+popupConfirm.addEventListener('click', handleModalClick);
+
+enableValidation(VALIDATION_CONFIG);
+
+Promise.all([APIGetUserInfo(), APIGetInitialCards()])
+  .then(([{ name, about, avatar, ['_id']: currentUserId }, cardsData]) => {
+    setProfile({
+      name,
+      description: about,
+      avatar,
+    });
+
+    cardsData.forEach((cardData) => {
+      cardsContainer.append(
+        DOMCreateCard({
+          currentUserId,
+          template: cardTemplate,
+          data: cardData,
+          onDelete: handleCardDelete,
+          onLike: handleCardLike,
+          onImageClick: handleCardImageClick,
+        })
+      );
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
